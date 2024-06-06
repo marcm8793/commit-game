7d48d95596e4c9b7001ae2be424f9b73bcb53d9c
vincent courault vcourault@eswarm.fr <vcourault@eswarm.fr>
2024-06-06 20:05:45 +0200

    model_arena ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_193429) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_175947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "arenas", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.text "image_url"
+    t.bigint "user_id", null: false
+    t.integer "prize"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_arenas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_193429) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "arenas", "users"
 end

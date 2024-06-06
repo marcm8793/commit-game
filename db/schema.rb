8880ff2d534ffcb731156be417adb53d982a26fc
vincent courault vcourault@eswarm.fr <vcourault@eswarm.fr>
2024-06-06 20:38:18 +0200

    gg wp

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_175947) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_181729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "arena_players", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "arena_id", null: false
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arena_id"], name: "index_arena_players_on_arena_id"
+    t.index ["user_id"], name: "index_arena_players_on_user_id"
+  end
 
   create_table "arenas", force: :cascade do |t|
     t.string "name"
@@ -27,6 +37,34 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_175947) do
     t.index ["user_id"], name: "index_arenas_on_user_id"
   end
 
+  create_table "commits", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_commits_on_project_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "arena_player_id", null: false
+    t.text "repo_url"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arena_player_id"], name: "index_projects_on_arena_player_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "score"
+    t.integer "week_number"
+    t.bigint "arena_player_id", null: false
+    t.boolean "done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arena_player_id"], name: "index_tasks_on_arena_player_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,5 +77,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_175947) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "arena_players", "arenas"
+  add_foreign_key "arena_players", "users"
   add_foreign_key "arenas", "users"
+  add_foreign_key "commits", "projects"
+  add_foreign_key "projects", "arena_players"
+  add_foreign_key "tasks", "arena_players"
 end

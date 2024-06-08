54182e82ece6bc503b1956d938d6f863bfcf38fa
Marc Mansour <marcm9387@gmail.com>
2024-06-08 17:09:50 +0200

    footer

--- app/models/arena_player.rb
+++ app/models/arena_player.rb
@@ -1,4 +1,12 @@
 class ArenaPlayer < ApplicationRecord
   belongs_to :user
   belongs_to :arena
+
+  validates :only_one_arena_player_per_user, on: :create
+
+  def only_one_arena_player_per_user
+    if ArenaPlayer.exists?(user_id: user_id)
+      errors.add(:user_id, "already has an arena player")
+    end
+  end
 end

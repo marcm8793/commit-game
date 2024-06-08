e0ee83350f020a30484914029641a5787c7f1620
Marc Mansour <marcm9387@gmail.com>
2024-06-08 13:04:11 +0200

    adding githubauth and avatar_url

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :pseudo, presence: true, uniqueness: true
+
+  devise :omniauthable, omniauth_providers: [:github]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+     unless user
+        user = User.create(
+           email: data['email'],
+           password: Devise.friendly_token[0,20],
+           pseudo: data['nickname'],
+           avatar_url: data['image']
+        )
+    end
+    user
+end
 end

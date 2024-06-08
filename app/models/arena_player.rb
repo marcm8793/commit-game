315ecac4fc3ec4209a15084976792688746af5d3
Marc Mansour <marcm9387@gmail.com>
2024-06-08 17:30:32 +0200

    seeds and validation

--- app/models/arena_player.rb
+++ app/models/arena_player.rb
@@ -2,11 +2,13 @@ class ArenaPlayer < ApplicationRecord
   belongs_to :user
   belongs_to :arena
 
-  validates :only_one_arena_player_per_user, on: :create
+  validate :only_one_arena_player_per_user_and_arena, on: :create
 
-  def only_one_arena_player_per_user
-    if ArenaPlayer.exists?(user_id: user_id)
-      errors.add(:user_id, "already has an arena player")
+  private
+
+  def only_one_arena_player_per_user_and_arena
+    if ArenaPlayer.exists?(user_id: user_id, arena_id: arena_id)
+      errors.add(:user_id, "already has an arena player for this arena")
     end
   end
 end

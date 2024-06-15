821cc123701cb47b87cc298d1c476a1860d8cd9b
Lucas Dimarellis <dimarellisl@gmail.com>
2024-06-15 16:16:08 +0200

    Test webhooks

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
-    # test
+    # testtest2
 
     # Uncomment the section below if you want users to be created if they don't exist
      unless user
